b06bc312104c7f1d680332e61cbd586e858264a0
stevenlii <stevenlii@qq.com>
2018-06-05 07:33:53 +0800

    first commit

---
+++ src/main/java/com/paymoon/com/shardjdbcdemo1/App.java
@@ -0,0 +1,13 @@
+package com.paymoon.com.shardjdbcdemo1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
